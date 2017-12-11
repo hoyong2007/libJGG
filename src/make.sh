7d7657842b09179fbe3c65cc1be8d4625557eaab
hoyong2007 <hoyong2007@naver.com>
2017-12-11 11:41:19 +0900

    Add -llog option on compile

--- src/make.sh
+++ src/make.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-~/arm32/bin/clang -shared -lm main.cpp unzip.cpp ioapi.cpp -lz -o libJGG.so 2>/dev/null
+~/arm32/bin/clang -shared -lm main.cpp unzip.cpp ioapi.cpp -lz -llog -o libJGG.so 2>/dev/null
 echo "build success!"
 
 python ../script/xor_patcher.py libJGG.so security 80
 python ../script/perm_patcher.py libJGG.so unpack
+python ../script/perm_patcher.py libJGG.so security
 echo "Patch Fin!"
 
-mv ./libJGG ../dummy/lib/armeabi-v7a/
+mv ./libJGG.so ../dummy/lib/armeabi-v7a/
 cd ..
 apktool b dummy/
 cp ./dummy/dist/dummy.apk ./

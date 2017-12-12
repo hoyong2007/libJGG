a7fbeb933ef30d59a921e2e62d27291099b256ee
hoyong2007 <hoyong2007@naver.com>
2017-12-12 21:10:33 +0900

    Update

--- script/check_info.py
+++ script/check_info.py
@@ -3,27 +3,24 @@
 import sys
 import subprocess
 
+if len(sys.argv) != 3:
+       print 'Usage : %s [ target ] [ symbol ]' % sys.argv[0]
 
-if __name__ == "__main__":
-    
-    if len(sys.argv) != 3:
-        print 'Usage : %s [ target ] [ symbol ]' % sys.argv[0]
-
-    else:
-		target = sys.argv[1]
-		symbol = sys.argv[2]
+else:
+	target = sys.argv[1]
+	symbol = sys.argv[2]
 	
-		# objdump를 이용해 target에서 symbol에 해당하는 함수의 offset과 length를 가져온다.
-		cmd = 'objdump -t %s | grep %s' % (target, symbol)
-		ret = subprocess.check_output(cmd, shell=True).split('\n')
-		for r in ret:
-			r = r.split()
-			if r[3] == ".text":
-				symbol = r[5]
-				offset = int(r[0], 16)
-				length = int(r[4], 16)
-				print "########################"
-				print "Symbol: %s" % symbol
-				print "Offset: 0x%x" % offset
-				print "Length: 0x%x" % length
-				break
+	# objdump를 이용해 target에서 symbol에 해당하는 함수의 offset과 length를 가져온다.
+	cmd = 'objdump -t %s | grep %s' % (target, symbol)
+	ret = subprocess.check_output(cmd, shell=True).split('\n')
+	for r in ret:
+		r = r.split()
+		if r[3] == ".text":
+			symbol = r[5]
+			offset = int(r[0], 16)
+			length = int(r[4], 16)
+			print "########################"
+			print "Symbol: %s" % symbol
+			print "Offset: 0x%x" % offset
+			print "Length: 0x%x" % length
+			break

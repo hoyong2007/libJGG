e98bca8465000d63799c636133af06237470ed3c
hoyong2007 <hoyong2007@naver.com>
2017-12-17 04:22:42 +0900

    remove log

--- src/libJGG.cpp
+++ src/libJGG.cpp
@@ -5,17 +5,12 @@ JavaVM *  s_vm = NULL;
 
 void unpack2()
 {
-    __android_log_print(ANDROID_LOG_INFO, "JGG", "Unpack");
-
     // xor encryption으로 패킹되어있는 함수 언패킹.
 	unpack2_f((unsigned char *)security, security_size);
-
-    __android_log_print(ANDROID_LOG_INFO, "JGG", "unpack - unpack finish");
 
     // securityCheck 함수 호출.
     security();
 
-    __android_log_print(ANDROID_LOG_INFO, "JGG", "unpack - FIN!");
     return;
 }
 
@@ -29,7 +24,6 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved)
 
     unpack_f((unsigned char *) security, security_size);
 
-    __android_log_print(ANDROID_LOG_INFO, "JGG", "JNI_OnLoad - unpack finish");
 
     // unpack 함수 호출.
     unpack2();
@@ -39,9 +33,7 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved)
 
     if(s_vm->GetEnv((void**) &env, JNI_VERSION_1_6) != JNI_OK)
     {
-        __android_log_print(ANDROID_LOG_INFO, "JGG", "JNI_VERSION_ERR");
         return JNI_ERR;
     }
-    __android_log_print(ANDROID_LOG_INFO, "JGG", "JNI_ONLOAD_FIN!");
     return JNI_VERSION_1_6;
 }
